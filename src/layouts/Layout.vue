83a2fa7aa1d24f880a3101b2204102988754e877
Kimmy <kimmy7526@gmail.com>
2025-04-09 16:21:49 +0800

    fix 畫面閃爍

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,14 +1,22 @@
 <template>
-    <div class="">
-        <Header></Header>
+    <div v-if="isMounted" class="flex flex-col min-h-screen">
+        <Header />
+        <main class="flex-1 py-24 bg-background text-text">
+            <RouterView />
+        </main>
+        <Footer />
     </div>
-    <div class="py-24 bg-background text-text">
-        <RouterView />
-    </div>
-    <Footer></Footer>
 </template>
 
 <script setup lang="ts">
-import Header from './Header.vue';
-import Footer from './Footer.vue';
+import { ref, onMounted } from 'vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+
+const isMounted = ref(false)
+
+onMounted(() => {
+    // 掛載完再顯示頁面
+    isMounted.value = true;
+})
 </script>
