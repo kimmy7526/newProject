d1b39ba9eec94195ec8df9c9d740fa07eb1549d1
Kimmy <kimmy7526@gmail.com>
2025-04-09 12:22:27 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-//  Vite 项目的配置文件
+// Vite 项目的配置文件
 export default defineConfig({
-  base: '/vue3-project/', // 用於 GitHub Pages 的子目錄配置
+  // 设置基础路径，部署到 GitHub Pages 的子路径
+  base: '/vue3-project/',
+
   plugins: [vue()],
+  
   resolve: {
     alias: {
       '@': '/src',
     },
-  }
-})
+  },
+});
